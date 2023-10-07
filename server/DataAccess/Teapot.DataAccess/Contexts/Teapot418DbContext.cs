36582b71d121b63e8506ff279104e24f1f08d5a9
Tunahan Karyagdi <thankaryagdi@gmail.com>
2023-10-07 11:35:59 +0300

    db set added

--- server/DataAccess/Teapot.DataAccess/Contexts/Teapot418DbContext.cs
+++ server/DataAccess/Teapot.DataAccess/Contexts/Teapot418DbContext.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Teapot.Entities.Concrete;
 
 namespace Teapot.DataAccess.Contexts
 {
     public class Teapot418DbContext : DbContext
     {
-        //public DbSet<> MyProperty { get; set; }
+       
+
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProject> UserProjects { get; set; }
+
+
+
     }
 }

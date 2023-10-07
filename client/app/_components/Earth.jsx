386022d6ca01d11c9aa6a899f242a856f04eb6f6
akifz21 <akifz2199@gmail.com>
2023-10-07 17:00:05 +0300

    up:earth

--- client/app/_components/Earth.jsx
+++ client/app/_components/Earth.jsx
@@ -2,13 +2,18 @@
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import React, { Suspense, useRef } from "react";
+import { USDZLoader } from "three/examples/jsm/loaders/USDZLoader";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Earth() {
-  const [color, normal, aoMap] = useLoader(TextureLoader, [
-    "/assets/color.jpg",
+  const [color, normal, aoMap, alpha, bump, spac] = useLoader(TextureLoader, [
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthmap1k.jpg",
     "/assets/normal.png",
     "/assets/occlusion.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthcloudmaptrans.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthbump1k.jpg",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthspec1k.jpg",
   ]);
 
   const meshRef = useRef();
@@ -20,19 +25,31 @@ function Earth() {
   return (
     <mesh ref={meshRef} scale={2.0}>
       <sphereGeometry args={[1, 64, 64]} />
-      <meshStandardMaterial map={color} normalMap={normal} aoMap={aoMap} />
+      <meshPhongMaterial
+        map={color}
+        normalMap={normal}
+        bumpMap={bump}
+        alphaMap={alpha}
+        specularMap={spac}
+        aoMap={aoMap}
+      />
     </mesh>
   );
 }
 
 export default function EartCanvas() {
-  const earth = useGLTF("./assets/Earth_1_12756.glb");
-
   return (
     <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={2.5} position={[1, 0.5, 0.25]} />
-      <primitive object={earth.asset} scale={2.5} />
+      <ambientLight intensity={0.7} />
+      <directionalLight intensity={4.5} position={[1, 0.75, 0.25]} />
+      <OrbitControls
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        enablePan={false}
+      />
+      <Earth />
     </Canvas>
   );
 }

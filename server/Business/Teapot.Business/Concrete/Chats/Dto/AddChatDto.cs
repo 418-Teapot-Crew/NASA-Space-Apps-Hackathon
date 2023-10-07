498037329ba753cc728f70836cfac78a541d3360
Tunahan Karyagdi <thankaryagdi@gmail.com>
2023-10-07 14:05:17 +0300

    services created

---
+++ server/Business/Teapot.Business/Concrete/Chats/Dto/AddChatDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teapot.Business.Concrete.Chats.Dto
+{
+    public class AddChatDto
+    {
+        public int ProjectOwnerId { get; set; }
+        public int ContributerId { get; set; }
+    }
+}

9e96e6f4e4cd2edae2dca5cb173d5b90ff9bb2e2
Tunahan Karyagdi <thankaryagdi@gmail.com>
2023-10-07 11:10:22 +0300

    entities created

---
+++ server/Entities/Teapot.Entities/Concrete/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teapot.Core.Entity.Abstract;
+
+namespace Teapot.Entities.Concrete
+{
+    public class Chat : IEntity
+    {
+        public int Id { get; set; }
+        public User ProjectOwner { get; set; }
+        public User Contributer { get; set; }
+        public ICollection<String> Messages { get; set; }   
+
+    }
+}

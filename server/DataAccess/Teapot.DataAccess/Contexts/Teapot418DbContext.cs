040b7707822eefe52546d621e74f4c36d69be539
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-10-08 00:20:29 +0300

    add db set

--- server/DataAccess/Teapot.DataAccess/Contexts/Teapot418DbContext.cs
+++ server/DataAccess/Teapot.DataAccess/Contexts/Teapot418DbContext.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Teapot.Core.Entities.Concrete;
 using Teapot.Entities.Concrete;
 
@@ -22,9 +17,6 @@ namespace Teapot.DataAccess.Contexts
         public DbSet<AppUser> Users { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
-
-
-
-
+        public DbSet<ProjectFile> ProjectFiles { get; set; }
     }
 }

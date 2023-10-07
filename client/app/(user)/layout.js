a35fe313e47e96c6af14e556e9eb22fef7548dc7
akifz21 <akifz2199@gmail.com>
2023-10-07 23:39:57 +0300

    ui:home

--- client/app/(user)/layout.js
+++ client/app/(user)/layout.js
@@ -11,7 +11,7 @@ export default function UserLayout({ children }) {
   return (
     <main className={spaceMono.className}>
       <Header />
-      <div className="">{children}</div>
+      <div className="px-24">{children}</div>
     </main>
   );
 }

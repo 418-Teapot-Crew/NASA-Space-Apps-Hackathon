99da15cc11d721f7b5b446ef68a9a092a365f8f6
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-10-07 11:12:02 +0300

    add db connection and web api project

---
+++ server/Teapot.WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Teapot.DataAccess.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Teapot418DbContext>(opt => opt
+                    .UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+                    .UseSnakeCaseNamingConvention()
+                    .EnableSensitiveDataLogging());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

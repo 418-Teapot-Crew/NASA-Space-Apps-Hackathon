5016bdba368da58fbb364b67fe46df2ab12220cd
akifz21 <akifz2199@gmail.com>
2023-10-08 14:07:33 +0300

    recomended projects

--- client/app/(user)/explore/page.jsx
+++ client/app/(user)/explore/page.jsx
@@ -1,24 +1,47 @@
 "use client";
 import Search from "../../_components/Search";
-import { getMockData } from "../../_api/user";
+import { getMockData, getUser } from "../../_api/user";
 import ExploreProjectCard from "../../_components/ExploreProjectCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Loading from "../../_components/Loading";
+import axios from "axios";
+import { useAuthContext } from "../../_contexts/AuthContext";
 
 const Explore = () => {
-  const data = getMockData();
+  const [data, setData] = useState([]);
   const [search, setSearch] = React.useState("");
   const [progress, setProgress] = React.useState(0);
+  const [user, setUser] = useState({});
+  const { state } = useAuthContext();
+
+  useEffect(() => {
+    getUser(state?.user?.id).then((res) => setUser(res.data?.data));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .post("https://multicoloredroundcad.uysalibov.repl.co/recommend", {
+        prompt: user?.description,
+      })
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+    console.log("aaaaaaaa");
+  }, [user]);
 
   return (
     <div className="pt-[130px] flex flex-col gap-10 items-center">
       <Loading progress={progress} setProgress={setProgress} />
-      <h1 className="font-light italic text-lg px-12">
-        "Unlocking the doors to a world of endless possibilities, our project
-        utilizes artificial intelligence to seamlessly connect users with Open
-        Science projects tailored to their unique profiles. Embrace the future
-        of discovery, where science meets individuality."
-      </h1>
+      <div className="flex flex-col gap-6 items-center justify-center">
+        <h1 className="font-bold text-4xl">
+          The projects we have chosen for you.
+        </h1>
+        <h1 className="font-light italic text-lg px-12">
+          "Unlocking the doors to a world of endless possibilities, our project
+          utilizes artificial intelligence to seamlessly connect users with Open
+          Science projects tailored to their unique profiles. Embrace the future
+          of discovery, where science meets individuality."
+        </h1>
+      </div>
 
       <div className="border-2 border-slate-300 flex w-full rounded">
         <input
@@ -33,8 +56,8 @@ const Explore = () => {
         </button>
       </div>
 
-      {data.projects.map((project) => (
-        <>{progress == 100 && <ExploreProjectCard />}</>
+      {data?.map((project) => (
+        <>{progress == 100 && <ExploreProjectCard project={project} />}</>
       ))}
     </div>
   );
--- client/app/_components/ExploreProjectCard.jsx
+++ client/app/_components/ExploreProjectCard.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import React from "react";
 import { motion } from "framer-motion";
 
-const ExploreProjectCard = () => {
+const ExploreProjectCard = ({ project }) => {
   const variants = {
     hidden: { opacity: 0, x: -200, y: 0 },
     enter: { opacity: 1, x: 0, y: 0 },
@@ -19,24 +19,24 @@ const ExploreProjectCard = () => {
       transition={{ type: "linear" }}
     >
       <Link
-        href="/"
-        className="flex h-60 items-center bg-white border border-gray-200 rounded-lg shadow flex-row w-full  hover:bg-gray-100 "
+        href={`/projects/${project?.project_id}`}
+        className="flex h-72 items-center justify-between bg-white border border-gray-200 rounded-lg shadow flex-row w-full  hover:bg-gray-100 "
       >
-        <div className="h-full w-1/3 relative">
-          <Image src={"/assets/bg.jpg"} fill alt="" />
+        <div className="h-full w-1/4 relative">
+          <Image src={"/assets/bg.jpg"} fill objectFit="cover" alt="" />
         </div>
-        <div className="flex flex-col justify-between p-4 leading-normal">
+        <div className="flex flex-col justify-between p-20 w-3/4 leading-normal">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-            Noteworthy technology acquisitions 2021
+            Name : {project?.project_name}
           </h5>
-          <p className="mb-3 font-normal text-gray-700 ">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
+          <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 ">
+            Location : {project?.geographic_scope}
+          </h5>
+          <p className="mb-3 line-clamp-3 font-normal text-gray-700 ">
+            {project?.project_description}
           </p>
-          <p className="italic">
-            Matched Skills : "Lorem, ipsum dolor sit amet consectetur
-            adipisicing elit. Error atque debitis ea est aut cupiditate itaque
-            dolorem fugit quasi nam".
+          <p className="italic line-clamp-1">
+            Matched Skills: {project.fields_of_science}
           </p>
           <h3 className="font-semibold ">John Doe</h3>
         </div>
